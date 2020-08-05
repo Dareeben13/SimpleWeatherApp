829814ac41faaad3a0cfb7e284f5912dd67cbab9
Eben <e2kelly2000@gmail.com>
2020-08-05 22:58:19 +0100

    final working weather app without my styles

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const API_KEY = '06166410947c156a21a672278f03ebc1'
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+export const FETCH_WEATHER = "FETCH_WEATHER"
+
+export const fetchWeather = (city) => {
+    const url = `${ROOT_URL}&q=${city},us`
+    const request = axios.get(url)
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+} 
